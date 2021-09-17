06495a1e65bfe397028c392f5d8227bcefb57ca2
dgtyPedro <pedrogcs0104@gmail.com>
2021-09-17 19:34:47 -0300

    Initial commit

---
+++ Appify/app/src/main/java/com/spotify_api_example/MainActivity.java
@@ -0,0 +1,90 @@
+package com.spotify_api_example;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify_api_example.Connectors.SongService;
+import com.spotify_api_example.Model.Song;
+import com.spotify_api_example.Model.User;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import static com.spotify.sdk.android.authentication.LoginActivity.REQUEST_CODE;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView userView;
+    private TextView songView;
+    private Button addBtn;
+    private Button skipBtn;
+    private Song song;
+
+    private SongService songService;
+    private ArrayList<Song> recentlyPlayedTracks;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        songService = new SongService(getApplicationContext());
+        userView = (TextView) findViewById(R.id.user);
+        songView = (TextView) findViewById(R.id.song);
+        addBtn = (Button) findViewById(R.id.add);
+        skipBtn = (Button) findViewById(R.id.skip);
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("SPOTIFY", 0);
+        userView.setText(sharedPreferences.getString("userid", "No User"));
+
+        getTracks();
+
+        addBtn.setOnClickListener(addListener);
+        skipBtn.setOnClickListener(skipListener);
+    }
+
+    private View.OnClickListener addListener = v -> {
+        songService.addSongToLibrary(this.song);
+        if (recentlyPlayedTracks.size() > 0) {
+            recentlyPlayedTracks.remove(0);
+        }
+        updateSong();
+    };
+
+
+    private void getTracks() {
+        songService.getRecentlyPlayedTracks(() -> {
+            recentlyPlayedTracks = songService.getSongs();
+            updateSong();
+        });
+    }
+
+    private View.OnClickListener skipListener = v -> {
+        if (recentlyPlayedTracks.size() > 0) {
+            recentlyPlayedTracks.remove(0);
+        }
+        updateSong();
+    };
+
+
+    private void updateSong() {
+        if (recentlyPlayedTracks.size() > 0) {
+            songView.setText(recentlyPlayedTracks.get(0).getName());
+            song = recentlyPlayedTracks.get(0);
+        }
+    }
+
+
+}
+
